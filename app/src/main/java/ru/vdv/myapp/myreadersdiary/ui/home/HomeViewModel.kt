94b358db3693f183d8a33b625a1079f68e197f9d
d.veremeenko <stdimensiy@yandex.ru>
2021-10-26 11:20:45 +0300

    Изм 0.2 - Рефакторинг базовой стурктуры UI и разметки

--- app/src/main/java/ru/vdv/myapp/myreadersdiary/ui/home/HomeViewModel.kt
+++ app/src/main/java/ru/vdv/myapp/myreadersdiary/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "Это главный фрагмент, тут размещается список книг"
     }
     val text: LiveData<String> = _text
 }
