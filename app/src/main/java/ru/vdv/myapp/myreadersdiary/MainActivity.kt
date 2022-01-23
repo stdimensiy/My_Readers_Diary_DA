72cafa6fac0676274e9aa0ce606fa70f448f39bb
Dmitriy <stdimensiy@yandex.ru>
2022-01-23 22:36:31 +0300

    Изм 1.14.3 Complex redesign UI - Полная переделка UI перевод на работу с нижним менб и акцент на работу с 1 кнопкой (90% операций должны будут выполняться нажатием на одну и туже кнопку без проблем с пониманием функционала)  -Убран лишний теперь DriVerLayout, изменен базовый файл разметки для активити  - небольшие изменения.

--- app/src/main/java/ru/vdv/myapp/myreadersdiary/MainActivity.kt
+++ app/src/main/java/ru/vdv/myapp/myreadersdiary/MainActivity.kt
@@ -14,38 +14,39 @@ import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
 import ru.vdv.myapp.myreadersdiary.databinding.ActivityMainBinding
+import ru.vdv.myapp.myreadersdiary.databinding.AppBarMainBinding
 import ru.vdv.myapp.myreadersdiary.ui.CustomBackButtonListener
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: AppBarMainBinding
     lateinit var fab: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = AppBarMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.appBarMain.toolbar)
+//        setSupportActionBar(binding.appBarMain.toolbar)
 
 
-        val bottomAppBar = binding.appBarMain.bottomAppBar
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navView: NavigationView = binding.navView
+        val bottomAppBar = binding.bottomAppBar
+//        val drawerLayout: DrawerLayout = binding.drawerLayout
+//        val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_new_main_fragment,
-                R.id.nav_list_of_books,
-                R.id.nav_summary_statistics
-            ), drawerLayout
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+//        appBarConfiguration = AppBarConfiguration(
+//            setOf(
+//                R.id.nav_new_main_fragment,
+//                R.id.nav_list_of_books,
+//                R.id.nav_summary_statistics
+//            ), drawerLayout
+//        )
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+//        navView.setupWithNavController(navController)
 
         bottomAppBar.setNavigationOnClickListener {
             Log.d("Моя проверка", "Нажата навигационная кнопка нижнего меню")
